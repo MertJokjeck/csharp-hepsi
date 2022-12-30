d0fbf5f3ba84589511b6adfed292d0b2dccbeafc
Jokjeck <119483588+MertJokjeck@users.noreply.github.com>
2022-12-30 12:06:38 +0300

    Add files via upload

---
+++ sayisalLoto1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int[] sayilar= new int[6];
+int[]pcSayilar=new int[6];
+int tahmin=0;
+
+Console.WriteLine("6 syı girin:");
+Random rnd=new Random();
+
+for (int i = 0; i < 6; i++)
+{
+    Console.WriteLine("{0}. Sayıyı Giriniz ???",(i+1));
+    sayilar[i]=int.Parse(Console.ReadLine()!);
+    pcSayilar[i]=rnd.Next(1,6);
+}
+foreach (var sayi in sayilar)
+{
+    foreach (var pcSayi in pcSayilar)
+    {
+        tahmin=sayi==pcSayi ? tahmin++: tahmin;
+    }
+}
+Console.WriteLine("{0} adet doğru bidin bravo ", tahmin);
+
---
+++ terstenYazma2/Program.cs
@@ -0,0 +1,5 @@
+﻿string ad="Mert";
+for (int i = ad.Length-1; i >=0; i--)
+{
+    Console.WriteLine(ad[i]);
+}
